5faf51befcb1be290a4b08cfa1616914c1817f41
Rafay Khan <131152047+RKAnalytics@users.noreply.github.com>
2026-06-15 00:18:16 +0500

    learned about datatypes

---
+++ 01_Basics/03_datatypes.js
@@ -0,0 +1,39 @@
+"use strict" //treat all js code as new version
+
+// alert("Hello, World!")
+// it will show an error beCAUSE WE ARE IN node js environment and alert is not defined in node js environment
+// OR
+// alert (3 +  3) // it will also show an error because of the same reason as above
+
+console.log (3 +
+    3
+) // bad formatting it will work but not a code practices
+
+let name = "Rafay"
+let age = 20
+let isLoggedIn = true
+let state;
+
+
+// Primitives data types in JavaScript
+// number => 2 to power 53
+// bigint 
+// string => '' or " " or ` `
+// boolean => true or false
+// null => it is an empty value (null is a stand alone value)
+// undefined => it is a value that is not defined yet
+// symbol => it is a unique identifier (when we talk about react)
+
+
+// objects in JavaScript
+
+// non primitive data types in JavaScript
+// object => it is a collection of key value pairs (it can store multiple values in it)
+// array => it is a collection of values (it can store multiple values in it)
+// function => it is a block of code that performs a specific task (it can store multiple values in it)
+
+
+console.log(typeof name) // it will print string
+// it will tell the type of data type of the variable
+console.log(typeof age) // it will print number
+console.log(typeof null) // it will print object
