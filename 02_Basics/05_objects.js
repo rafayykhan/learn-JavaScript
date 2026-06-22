ec10390b15efeb4743306981075b135570d5fb63
Rafay Khan <131152047+RKAnalytics@users.noreply.github.com>
2026-06-22 19:40:48 +0500

    Completed the objects

--- 02_Basics/05_objects.js
+++ 02_Basics/05_objects.js
@@ -1,1 +1,43 @@
+// destructuring of objects 
 
+const course = {
+    coursename: "Data",
+    price: "123",
+    courseInstructor: "Rfaya"
+}
+
+// course.courseInstructor
+
+// to make data clean
+
+// from where we have to extract the value
+const {/* from which property and we can also rename this  */ courseInstructor: inst} = course // from this  object
+
+console.log(inst) // return the name of the instructor
+
+
+// react stuff 
+// const navbar = (company) => {
+
+// }
+// navbar(company = "rafay")
+
+
+// API ---> json format
+
+// values came  from backen in the form of JSON
+
+// {
+//     name: "RAFAY",
+//     courseName : "JS",
+//     price: "free",
+// }
+
+// API can be in the orm if ann array which contain many objects
+
+// [
+//     {},
+//     {},
+//     {},
+//     {},
+// ]
