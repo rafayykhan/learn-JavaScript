79ba96efe65a1edf40e7fe0a6c9e3d5c1fffb68f
Rafay Khan <131152047+RKAnalytics@users.noreply.github.com>
2026-06-26 02:24:21 +0500

    conpleted the ifElse

---
+++ 04_ControlFlow/01_ifElse.js
@@ -0,0 +1,44 @@
+// if
+
+// if(true){
+//     // this code will execute when it is true
+// }
+
+// if(false){
+//     // this code will execute when it is false
+// }
+
+//        Comparision Operator      //
+/*
+
+< less then
+> greater then
+>= greter then equal to
+<= less than equal to 
+== comparision Operator
+!= not equal to
+=== it also compare the type -> called strict equal 
+!==  strict not checking
+
+*/
+
+const temp = 60
+
+if (temp < 50) {
+    console.log("temperature is less than 50")
+} else {
+    console.log("temperature is greater than 50")
+
+}
+
+// short hand notation 
+
+const balance = 1000
+
+if (balance > 500) console.log("test"); // implicit scope   
+
+// Operators
+//  && AND operator all conditions must be true
+//  || OR operator one of all conditions must be true
+
+
